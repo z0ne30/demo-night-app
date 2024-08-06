a698ed7d19631c14337c37ca8a778d4bec41f603
Enyu Rao <101046111+z0ne30@users.noreply.github.com>
2024-08-06 16:46:32 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
 /**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * Run build or dev with SKIP_ENV_VALIDATION to skip env validation. This is especially useful
  * for Docker builds.
  */
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {
-  experimental: {
-    speedInsights: true, // Enable SpeedInsights
-  },
-  // Other Next.js config options can be added here
-};
+const config = {};
 
-export default SpeedInsights(config);
+export default config;
